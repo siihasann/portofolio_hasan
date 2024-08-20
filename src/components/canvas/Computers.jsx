28dc4fc5f3dcb75bc5fdd9bfc516e0719ee14830
siihasann <hasannajib3@gmail.com>
2024-08-20 17:04:27 +0700

    first commit

---
+++ src/components/canvas/Computers.jsx
@@ -0,0 +1,71 @@
+import { Suspense, useEffect, useState } from "react";
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+
+import CanvasLoader from '../Loader';
+
+const Computers = ({isMobile}) => { 
+  const computer = useGLTF('./planet/scene.gltf');
+  return (
+    <mesh>
+      <hemisphereLight intensity={2}
+        groundColor="black" />
+      <pointLight intensity={1}
+      position={[0, -0.3, 0]}/>
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+      />
+      <primitive
+        object={computer.scene}
+        // scale={ isMobile ? 0.6 : 0.55}
+        scale={ isMobile ? 2.5 : 2}
+        // position={isMobile ? [0, -2.9, -2.2] : [0, -2.7, -1.5]}
+        position={isMobile ? [0, -1.5, -0.2] : [0, -1.5, -0.2]}
+        rotation={[-0.01, -0.2, -0.1]}
+      />
+    </mesh>
+  )
+}
+
+const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuary = window.matchMedia('(max-width: 500px)');
+
+    setIsMobile(mediaQuary.matches);
+
+    const handleMediaQuaryChange = (event) => {
+      setIsMobile(event.matches);
+    }
+
+    mediaQuary.addEventListener('change', handleMediaQuaryChange);
+
+    return () => {
+      mediaQuary.removeEventListener('change', handleMediaQuaryChange);
+    }
+    
+  }, [])
+
+  return (
+    <Canvas
+      frameloop="demand"
+      shadows={false}
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+          dampingFactor={0.05}
+        />
+        <Computers isMobile={isMobile} />
+      </Suspense>
+      <Preload all />
+    </Canvas>
+  );
+};
+export default ComputersCanvas;

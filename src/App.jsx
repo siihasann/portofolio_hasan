02d35a7eae1a318acccf257e9746fe493d1e07e6
siihasann <hasannajib3@gmail.com>
2024-08-21 10:40:12 +0700

    coba

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <div className="bg-primary">
-        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+        <div className="bg-cover bg-no-repeat bg-center">
           <Navbar />
           <div className='relative z-0'>
             <StarsCanvas />
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,10 +8,10 @@ const Hero = () => {
       <div
         className={`${styles.paddingX} absolute inset-0 top-[100px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
       >
-        <div className="flex flex-col justify-center items-center mt-5">
+        {/* <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#915eff]" />
           <div className="w-1 sm:h-80 h-40 violet-gradient" />
-        </div>
+        </div> */}
         <div>
           <h1 className={`${styles.heroHeadText}`}>
             Hi, I'm <span className="text-[#915eff]">Hasan Najib</span>
